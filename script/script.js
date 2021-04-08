7fe06a3c162c19c790066d9d5bf8b348a1cb2901
kangchanmi <alslrkdcks@naver.com>
2021-04-08 13:49:14 +0900

    0408_영어/숫자/특수기호 유효성 검사

--- script/script.js
+++ script/script.js
@@ -3,10 +3,11 @@ $(function () {
     $("button").click(function () {
 
         var chkData = $("#insData").val(); // 비교 내용 1 / input 내용
+
         chkData = chkData.trim(); // 앞뒤 공백 제거
 
         // 비교 기준
-        var pattern = "한글 유효성 검사"; // 비교 내용 2  / 패턴 (비교기준)
+        var pattern = "한글 유효성 검사";
         var regExp = /[^ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/;
         // var regExp = new RegExp(pattern);
         var res = "";
@@ -17,7 +18,6 @@ $(function () {
         } else {
             res = "올바른 입력입니다.";
         }
-
 
         $("#chkPattern").text("패턴(비교기준) : " + pattern)
         $("#res").text("패턴 비교 결과 : " + res);
